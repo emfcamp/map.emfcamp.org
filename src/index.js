fcfcfb0be2c95831987e7dc4083eb3fdbd099e3a
Russ Garrett <russ@garrett.co.uk>
2018-03-04 15:51:08 +0000

    Engage the hipster JS hilarity

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
+import mapboxgl from 'mapbox-gl';
+import map_style from './map_style.json';
+
 mapboxgl.accessToken = 'undefined';
+
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'emf.json',
+    style: map_style,
     hash: true
 });
 
 map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
 map.addControl(new mapboxgl.GeolocateControl({
     positionOptions: {
         enableHighAccuracy: true
     },
     trackUserLocation: true
 }));
+
 map.addControl(new mapboxgl.ScaleControl({
-    maxWidth: 80,
+    maxWidth: 120,
     unit: 'metric'
 }));
---
+++ webpack.common.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
