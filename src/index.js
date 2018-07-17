917a2bd1be14758b0a020e2cef28d335778ff37b
dpslwk <dps.lwk@gmail.com>
2018-07-17 14:01:41 +0100

    Add power layers

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ map.addControl(new DistanceMeasure(), 'top-right');
 var layers = {
     'Buried Services': 'services_',
     'Water': 'site_water_',
-    'NOC': 'noc_'
+    'NOC': 'noc_',
+    'Power': 'power_'
 }
 map.addControl(new LayerSwitcher(layers), 'top-right');
